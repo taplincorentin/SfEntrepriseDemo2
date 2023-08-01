45e3cc8987373a99305f0cf09d1eb0aa0d33a75b
corentin <taplincorentin@gmail.com>
2023-08-01 17:00:05 +0200

    made ville not required in form

--- src/Form/EmployeType.php
+++ src/Form/EmployeType.php
@@ -40,6 +40,7 @@ class EmployeType extends AbstractType
                 ]
             ])
             ->add('ville', TextType::class, [
+                'required' => false,
                 'attr' => [
                     'class' => 'form-control'
                 ]
